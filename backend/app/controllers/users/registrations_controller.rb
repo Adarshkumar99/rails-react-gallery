9d369922771c97e2219192f505198c93b85ca68e
Adarsh Kumar <adarshrajputhjp@gmail.com>
2026-05-17 13:34:19 +0200

    fix devise-jwt token issue in sign in

--- backend/app/controllers/users/registrations_controller.rb
+++ backend/app/controllers/users/registrations_controller.rb
@@ -1,85 +1,32 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
   respond_to :json
 
-  # GET /resource/sign_up
-  # def new
-  #   super
-  # end
-
-  # POST /resource
   def create
-    super
+    user = User.new(sign_up_params)
+
+    if user.save
+      token = Warden::JWTAuth::UserEncoder.new.call(user, :user, nil).first
+      response.set_header('Authorization', "Bearer #{token}")
+      render json: {
+        message: 'Signed up successfully',
+        user: { id: user.id, email: user.email }
+      }, status: :created
+    else
+      render json: {
+        errors: user.errors.full_messages
+      }, status: :unprocessable_entity
+    end
   rescue ActiveRecord::RecordNotUnique
     render json: {
       errors: ["Email already taken."]
     }, status: :unprocessable_entity
   end
 
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
-
-  # PUT /resource
-  # def update
-  #   super
-  # end
-
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
-
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
-
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
-
-  # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
-  # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
-
   private
 
-  def sign_up(resource_name, resource)
-  end
-
-  def respond_with(resource, _opts = {})
-    if resource.persisted?
-      render json: {
-        message: 'Signed up successfully',
-        user: resource
-      }, status: :ok
-    else
-      render json: {
-        errors: resource.errors.full_messages
-      }, status: :unprocessable_entity
-    end
+  def sign_up_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
